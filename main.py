fda747fd4f7aa9135a2f262aadb4782e091b176d
marshadkhalil <marshadkhalil@outlook.com>
2026-01-06 00:34:50 +0500

    Add simple langchain chain

--- main.py
+++ main.py
@@ -1,10 +1,22 @@
 from dotenv import load_dotenv
+from langchain_core.prompts import PromptTemplate
+from langchain_openai import ChatOpenAI
 
 load_dotenv()
 
 
 def main():
-    print("Hello from langchain-agents!")
+    city = "Lahore"
+    summary_template = "tell me weather of {city}"
+    summary_prompt_template = PromptTemplate(
+        input_variables=["city"],template=summary_template
+    )
+
+    llm = ChatOpenAI(temperature=0,model="gpt-5")
+    chain = summary_prompt_template | llm
+    response = chain.invoke(input={"city":city})
+    print(response.content)
+
 
 
 if __name__ == "__main__":
